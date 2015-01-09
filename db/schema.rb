4be58c25d64ca250133abc7d3e2f64d43e877caf
Domas <domas.bitvinskas@me.com>
2015-01-09 22:46:39 +0200

    Remove email from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108220744) do
+ActiveRecord::Schema.define(version: 20150109204523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
